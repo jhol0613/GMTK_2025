4f3681f40aa99002fb8ab5dd8c00f2037dbbb23e
jhol0613 <john.hollandr@gmail.com>
2025-07-30 20:26:31 -0700

    Some work on player character and game structure

---
+++ godot_project/environment/tilemap_level.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var bounds : Vector2i
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func get_traversible_neighbors(position: Vector2i) -> Array[Vector2i]:
+	var neighbors = $Obstacles.get_used_cells()
+	var neighbors_to_return : Array[Vector2i]
+	
+	for neighbor in neighbors:
+		if $Obstacles.get_cell_tile_data().get_custom_data("Traversible"):
+			neighbors_to_return.append(neighbor)
+		
+	return neighbors_to_return
+	
