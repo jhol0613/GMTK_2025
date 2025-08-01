db85ace6de25673e6d91358f43c6b3a5dcebd951
sasetz <56696451a@gmail.com>
2025-08-01 16:06:15 +0200

    Add basic conductor setup

--- godot_project/tilemap_base/tilemap_level_base.gd
+++ godot_project/tilemap_base/tilemap_level_base.gd
@@ -8,37 +8,35 @@ class_name TilemapLevel
 @onready var _obstacle_layer : TileMapLayer = $Obstacles
 @onready var _wall_layer : TileMapLayer = $BackWall
 
-# Called when the node enters the scene tree for the first time.
+var path_grid: AStarGrid2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	path_grid = AStarGrid2D.new()
+	path_grid.region = Rect2i()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
 func get_traversible_neighbors(grid_position: Vector2i) -> Array[Vector2i]:
 	var neighbors = _obstacle_layer.get_surrounding_cells(grid_position)
 	var neighbors_to_return : Array[Vector2i]
-	
+
 	for neighbor in neighbors:
 		var cell_exists = (_floor_layer.get_cell_source_id(neighbor) != -1 and
 			_floor_layer.get_cell_atlas_coords(neighbor) != Vector2i(-1,-1))
-		
+
 		var tile_data = _obstacle_layer.get_cell_tile_data(neighbor)
 		var cell_traversible = true # Traversible if the cell is unoccupied
-		
+
 		if tile_data:
 			cell_traversible = tile_data.get_custom_data("Traversible")
-		
+
 		if cell_exists and cell_traversible:
 			neighbors_to_return.append(neighbor)
-		
+
 	return neighbors_to_return
-	
+
 func get_vertical_tile_spacing():
 	return _floor_layer.tile_set.tile_size.y
-	
+
 func get_horizontal_tile_spacing():
 	return _floor_layer.tile_set.tile_size.x
 	
