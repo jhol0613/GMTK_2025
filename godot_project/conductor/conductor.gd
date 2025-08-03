9e62ce425c06ee64463b508df76ee16efe1d1be6
jhol0613 <john.hollandr@gmail.com>
2025-08-02 19:47:10 -0700

    Conductor visuals

--- godot_project/conductor/conductor.gd
+++ godot_project/conductor/conductor.gd
@@ -5,6 +5,27 @@ class_name Conductor
 
 signal player_caught
 
+@onready var jump_emitter = $Sound/JumpSound
+@onready var entered_emitter = $Sound/EnteredSound
+@onready var voice_emitter = $Sound/Voice
+
+func _ready():
+	super._ready()
+	action_executed.connect(_on_action_executed)
+
+func _on_action_executed(action: Enums.PlayerAction) -> void:
+	var emitter: FmodEventEmitter2D = null
+	match action:
+		Enums.PlayerAction.RIGHT, Enums.PlayerAction.UP, Enums.PlayerAction.DOWN:
+			sprite.flip_h = false
+			emitter = jump_emitter
+		Enums.PlayerAction.LEFT:
+			sprite.flip_h = true
+			emitter = jump_emitter
+	
+	if emitter == null: # in case the action is not a movement action
+		return
+	emitter.play()
 
 func _on_player_entered(area: Area2D) -> void:
 	player_caught.emit()
