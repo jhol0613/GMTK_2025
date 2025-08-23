e521c7d52110ba4e997884e07332d35986313c82
LangyiRen <144127676+LangyiRen@users.noreply.github.com>
2025-08-22 22:51:57 -0500

    pause menu

--- godot_project/menus/pause_menu/pause_menu.gd
+++ godot_project/menus/pause_menu/pause_menu.gd
@@ -1,24 +1,101 @@
 extends Control
 
-func resume():
-	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
+@onready var pause       : Control      = $pause_menu
+@onready var bg          : TextureRect  = $pause_menu/pause_menu_bg
+@onready var confirm     : Control      = $pause_menu/quit_confirm
+@onready var confirm_bg  : TextureRect  = $pause_menu/quit_confirm/confirm_menu_bg
+@onready var shade       : ColorRect    = $shade
 
-func pause():
-	get_tree().paused = true
-	$AnimationPlayer.play("blur")
-	
-func testEsc():
-	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
-		pause()
-	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
-		resume()
+const T_PAUSE        := preload("res://menus/pause_menu/pausemenu_visuals/pause.png")
+const T_RESUME_HI    := preload("res://menus/pause_menu/pausemenu_visuals/pauseresume.png")
+const T_OPTIONS_HI   := preload("res://menus/pause_menu/pausemenu_visuals/pauseoptions.png")
+const T_EXIT_HI      := preload("res://menus/pause_menu/pausemenu_visuals/pausexit.png")
+
+const T_YESNO        := preload("res://menus/pause_menu/pausemenu_visuals/yesno.png")
+const T_YES_HI       := preload("res://menus/pause_menu/pausemenu_visuals/yes.png")
+const T_NO_HI        := preload("res://menus/pause_menu/pausemenu_visuals/no.png")
+
+var _is_open := false
+
+func _ready() -> void:
+	visible = false
+	pause.visible = false
+	confirm.visible = false
+	shade.visible = false
+  
+	$pause_menu/resume_btn.mouse_entered.connect(_on_resume_btn_mouse_entered)
+	$pause_menu/options_btn.mouse_entered.connect(_on_options_btn_mouse_entered)
+	$pause_menu/quit_btn.mouse_entered.connect(_on_quit_btn_mouse_entered)
+
+	$pause_menu/resume_btn.mouse_exited.connect(_on_main_exit_hover_clear)
+	$pause_menu/options_btn.mouse_exited.connect(_on_main_exit_hover_clear)
+	$pause_menu/quit_btn.mouse_exited.connect(_on_main_exit_hover_clear)
+
+	$pause_menu/resume_btn.pressed.connect(_on_resume_btn_pressed)
+	$pause_menu/options_btn.pressed.connect(_on_options_btn_pressed)
+	$pause_menu/quit_btn.pressed.connect(_on_quit_btn_pressed)
+
+	$pause_menu/quit_confirm/quit_yes.mouse_entered.connect(func(): confirm_bg.texture = T_YES_HI)
+	$pause_menu/quit_confirm/quit_no.mouse_entered.connect(func(): confirm_bg.texture = T_NO_HI)
+	$pause_menu/quit_confirm/quit_yes.mouse_exited.connect(func(): confirm_bg.texture = T_YESNO)
+	$pause_menu/quit_confirm/quit_no.mouse_exited.connect(func(): confirm_bg.texture = T_YESNO)
+
+	$pause_menu/quit_confirm/quit_yes.pressed.connect(_on_quit_yes_pressed)
+	$pause_menu/quit_confirm/quit_no.pressed.connect(_on_quit_no_pressed)
+
+func toggle_pause() -> void:
+	_is_open = !_is_open
+	get_tree().paused = _is_open
+	visible = _is_open
+	pause.visible = _is_open
+	shade.visible = _is_open
+	confirm.visible = false
+	if _is_open:
+		bg.texture = T_PAUSE
+	else:
+		bg.texture = T_PAUSE
 		
-func _on_resume_pressed() -> void:
-	resume()
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		toggle_pause()
+		get_viewport().set_input_as_handled()
+		
+func _on_resume_btn_mouse_entered() -> void:
+	if not confirm.visible:
+		bg.texture = T_RESUME_HI
 
-func _on_quit_pressed() -> void:
+
+func _on_options_btn_mouse_entered() -> void:
+	if not confirm.visible:
+		bg.texture = T_OPTIONS_HI
+
+
+func _on_quit_btn_mouse_entered() -> void:
+	if not confirm.visible:
+		bg.texture = T_EXIT_HI
+
+func _on_main_exit_hover_clear() -> void:
+	if not confirm.visible:
+		bg.texture = T_PAUSE
+
+func _on_resume_btn_pressed() -> void:
+	toggle_pause()
+
+
+func _on_options_btn_pressed() -> void:
+	bg.texture = T_PAUSE
+	# Todo: add a options menu here
+
+
+func _on_quit_btn_pressed() -> void:
+	confirm.visible = true
+	confirm_bg.texture = T_YESNO
+
+
+func _on_quit_yes_pressed() -> void:
 	get_tree().quit()
 
-func _process(delta):
-	testEsc()
+
+func _on_quit_no_pressed() -> void:
+	confirm.visible = false
+	bg.texture = T_PAUSE
