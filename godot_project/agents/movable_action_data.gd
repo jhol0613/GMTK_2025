9505291d80965024b02ee6fd477e7c693b7f7298
jhol0613 <john.hollandr@gmail.com>
2025-11-07 08:39:58 +0300

    Updated some agents to use the new system

--- godot_project/agents/movable_action_data.gd
+++ godot_project/agents/movable_action_data.gd
@@ -6,7 +6,8 @@ class_name MovableActionData
 ## position (0.0 is old grid space, 1.0 is new grid space)
 @export var direct_movement_curve: Curve
 ## The y value the movable should add to their movement as they move to another tile (to add a 
-## "jumping" component instead of just linear motion). Should end on 0.0 to return to original altitude
+## "jumping" component instead of just linear motion). Should end on 0.0 to return to original altitude.
+## If undefined, 0 curve is used.
 @export var y_movement_curve: Curve
 @export var y_movement_magnitude := 16.0
 ## Amount of time for movement animation
