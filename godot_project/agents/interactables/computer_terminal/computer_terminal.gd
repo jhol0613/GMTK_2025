320fb72fd7d2b970fe2ef5d9caaa70ef42e04b62
jhol0613 <john.hollandr@gmail.com>
2026-02-26 07:32:50 +0300

    Implemented change chair direction program, removed ability to click on terminals

--- godot_project/agents/interactables/computer_terminal/computer_terminal.gd
+++ godot_project/agents/interactables/computer_terminal/computer_terminal.gd
@@ -6,10 +6,6 @@ class_name Terminal
 
 ##Number of beats before action occurs after player starts terminal interaction, since it would take time to type and execute
 @export var input_delay_beats := 2.0
-@export var selectable := false
-##Needs to be set in code for values over 1,1,1 (to lighten)
-@export var mouse_hover_modulate := Color(1.25, 1.25, 1.25)
-@export var select_button : Button
 var programs : Array[TerminalProgram]
 
 signal selected(programs: Array[TerminalProgram])
@@ -24,22 +20,6 @@ func _ready() -> void:
 	for child in get_children():
 		if child is TerminalProgram:
 			programs.append(child)
-	select_button.disabled = not selectable
-	if selectable:
-		select_button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
-	else:
-		select_button.mouse_default_cursor_shape = Control.CURSOR_ARROW
-
-func _on_mouse_entered():
-	if selectable:
-		sprite.modulate = mouse_hover_modulate
-
-func _on_mouse_exited():
-	sprite.modulate = Color(1,1,1,1)
-
-func _on_clicked() -> void:
-	if selectable:
-		selected.emit(programs)
 
 # Ensure terminal program is configured as a child node
 func _get_configuration_warnings() -> PackedStringArray:
--- godot_project/agents/interactables/computer_terminal/terminal_programs/turn_chair_program.gd
+++ godot_project/agents/interactables/computer_terminal/terminal_programs/turn_chair_program.gd
@@ -5,9 +5,12 @@ class_name TurnChairProgram
 @export var chairs : Array[SpinningChair]
 @export var number_of_quarter_turns := 2
 
+var _previously_selected_direction
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	sequencer_control_scene_UID = "uid://crorf1owblj5p"
+	super._ready()
 
 func run():
 	for chair in chairs:
@@ -15,3 +18,25 @@ func run():
 
 func reset():
 	pass
+
+func initialize_screen(screen_scene: TurnChairScreen):
+	super.initialize_screen(screen_scene)
+	if screen_scene is not TurnChairScreen:
+		push_error("Ensure that change_laser_direction_program has change_laser_direction_screen ans its
+		sequencer control scene")
+	screen_scene.direction_pressed.connect(_on_direction_selected)
+
+func _on_direction_selected(direction: Enums.Direction):
+	for chair in chairs:
+		if direction == _previously_selected_direction:
+			break
+		elif direction == Enums.Direction.LEFT and chair._facing_direction == chair.FacingDirection.FRONT_COUNTERCLOCKWISE:
+			chair.start_direction = chair.FacingDirection.FRONT_CLOCKWISE
+		elif direction == Enums.Direction.RIGHT and chair._facing_direction == chair.FacingDirection.FRONT_CLOCKWISE:
+			chair.start_direction = chair.FacingDirection.FRONT_COUNTERCLOCKWISE
+		chair.spin(chair.get_quarter_turns_to_direction(direction))
+	_previously_selected_direction = direction
+
+func _determine_quarter_spins(direction: Enums.Direction):
+	pass
+	#if direction == Enums.Direction.DOWN and _previously_selected_direction != Enums.Direction.DOWN
