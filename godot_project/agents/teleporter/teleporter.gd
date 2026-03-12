9c5b39b471dd2f38490cb149d61359b7ac28a9cd
jhol0613 <john.hollandr@gmail.com>
2026-03-12 04:05:12 +0300

    commiting teleporter visuals for pulling sfx updates

--- godot_project/agents/teleporter/teleporter.gd
+++ godot_project/agents/teleporter/teleporter.gd
@@ -10,16 +10,21 @@ class_name Teleporter
 		destination = _validate_destination(new_destination)
 		_update_target_position()
 ##The downbeat is beat 0.0
-@export var push_beat := 2.0
 @export var move_mode := Enums.MoveMode.ON_BEAT
+@export var top : TeleporterTop
+@export var initial_target_sprite_offset := Vector2(0, 0)
 
 
 @onready var sound := $FmodEventEmitter2D
 @onready var collision := $Area2D
-@onready var target_sprite := $TargetSprite
+@onready var base_sprite := $Node2D/TeleporterBaseFront
+@onready var target_sprite : AnimatedSprite2DSignals = $Target/YSortOffset/TargetSprite
 
 # set in runtime, found dynamically using its name
 var _level_scene: RhythmRailLevel
+
+##time in seconds that warmup animation should start before actual teleportation happens
+var _animation_offset: float
 
 # Editor-only variables
 
@@ -31,6 +36,11 @@ func _ready() -> void:
 	add_to_group("teleporters")
 	if not Engine.is_editor_hint():
 		AudioManager.music_bar.connect(_on_music_bar)
+	if not top:
+		return
+	if AudioManager.beat_time_seconds * default_action_beat < top.sprite.default_animation_offset:
+		push_warning("Teleporter powerup animation can't be triggered because it would bleed into the
+		previous bar. Either reduce teleporter top animation offset or move teleporter action beat later")
 
 #region Editor-only reticle
 
@@ -54,21 +64,41 @@ func _validate_destination(new_destination: Vector2i) -> Vector2i:
 func _update_target_position() -> void:
 	if not target_sprite:
 		return
-	target_sprite.position = Vector2((destination - grid_origin) * tile_size)
+	target_sprite.position = Vector2((destination - grid_origin) * tile_size) + initial_target_sprite_offset
 
 
 #endregion
 
 func _teleport(entity: Movable) -> void:
+	if entity is PlayerCharacter: #special case for player character since we have art for it
+		entity.visible = false
+		base_sprite.play_with_signals("player_teleport")
+		await base_sprite.animation_finished
+		base_sprite.play_with_signals("default")
 	# TODO: add animation
 	# TODO: add sound
 	#print("Queueing teleport for %s" % entity)
 	entity.queue_teleportation(destination)
+	_play_respawn_animation()
+	entity.visible = true
 
+##Play this animation at teleporter origin when something gets teleported
+func _play_despawn_animation():
+	pass
+ 
+##Play this animation at teleporter target on getting teleported to destination
+func _play_respawn_animation():
+	target_sprite.play_with_signals("respawn")
 
 func _on_music_bar():
 	if move_mode == Enums.MoveMode.ON_BEAT:
-		get_tree().create_timer(push_beat * AudioManager.beat_time_seconds).timeout.connect(_on_push_beat_timeout)
+		if top:
+			await get_tree().create_timer(default_action_beat * AudioManager.beat_time_seconds + top.sprite.default_animation_offset).timeout
+			top.sprite.play_with_signals("power_up")
+			await top.sprite.animation_signal #top sprite should only emit one signal, so no need to check signal id
+			_on_push_beat_timeout()
+		else:
+			get_tree().create_timer(default_action_beat * AudioManager.beat_time_seconds).timeout.connect(_on_push_beat_timeout)
 	else:
 		_on_push_beat_timeout()
 
---
+++ godot_project/agents/teleporter/teleporter_top.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+##Visuals for top part of a teleporter. Has no functionality except animation
+class_name TeleporterTop
+
+@export var sprite : AnimatedSprite2DSignals
+
+#func play_teleport_animation():
+	#sprite.play_with_signals("teleport")
+#
+###Animation should start this long before the actual teleportation
+#func get_animation_offset():
+	#return sprite.default_animation_offset
