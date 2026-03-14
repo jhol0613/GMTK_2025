962011c197ac28d397d494facea3f3a10cdcb1e6
LangyiRen <144127676+LangyiRen@users.noreply.github.com>
2026-03-14 17:54:52 -0500

    SFX for newspaper open/close implemented; FMod event for duck, cart bumping, updated

--- godot_project/agents/interactables/hide_bench/hide_bench.gd
+++ godot_project/agents/interactables/hide_bench/hide_bench.gd
@@ -5,6 +5,9 @@ class_name HideBench
 # Number of actions to hide
 @export var hide_duration := 3
 
+@onready var sound_open := $FmodEventEmitter2D
+@onready var sound_close := $FmodEventEmitter2D2
+
 var hiding := false
 var beats_hidden := 0
 
@@ -26,12 +29,14 @@ func _on_interaction_succeeded():
 	
 	
 func _start_hiding():
+	sound_open.play()
 	player_animation_on_success = "unhide"
 	follow_on_animation_on_success = "idle_down"
 	hiding = true
 	started_hiding.emit()
 
 func _finish_hiding():
+	sound_close.play()
 	player_animation_on_success = "hide"
 	follow_on_animation_on_success = "hiding"
 	hiding = false
