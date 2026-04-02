80f85bf167686705738a6c0a63a9dee7b484c208
jhol0613 <john.hollandr@gmail.com>
2026-04-02 04:27:30 +0300

    Level 3-7 and bug fix where treadmill direction wasn't resetting

--- godot_project/agents/interactables/computer_terminal/terminal_programs/change_treadmill_direction_program.gd
+++ godot_project/agents/interactables/computer_terminal/terminal_programs/change_treadmill_direction_program.gd
@@ -5,9 +5,13 @@ class_name TurnTreadmillProgram
 @export var treadmills : Array[Treadmill]
 @export var new_direction : Enums.Direction
 
+var original_directions : Dictionary[Treadmill, Enums.Direction]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sequencer_control_scene_UID = "uid://c0rrg37ad7473"
+	for treadmill in treadmills:
+		original_directions[treadmill] = treadmill.direction
 	super._ready()
 
 func initialize_screen(screen_scene: ChangeTreadmillDirectionScreen):
@@ -23,4 +27,5 @@ func run():
 		treadmill.direction = new_direction
 
 func reset():
-	pass
+	for treadmill in treadmills:
+		treadmill.direction = original_directions[treadmill]
