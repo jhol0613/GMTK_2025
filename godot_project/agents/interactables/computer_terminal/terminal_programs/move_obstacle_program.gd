449f7bee053547e020585139edbdfa297c5cf237
jhol0613 <john.hollandr@gmail.com>
2026-04-04 01:03:38 +0300

    Level 3-8. Also added ability to override moving obstacle reset behaviors

--- godot_project/agents/interactables/computer_terminal/terminal_programs/move_obstacle_program.gd
+++ godot_project/agents/interactables/computer_terminal/terminal_programs/move_obstacle_program.gd
@@ -5,6 +5,15 @@ class_name MoveObstacleProgram
 @export var obstacles : Array[MovableObstacle]
 ##Action to take when program is run
 @export var action := Enums.PlayerAction.LEFT
+##Default is auto, where obstacle will reset position if it was moved via terminal or maintain its position
+##if moved via antenna.
+@export var should_reset_position := PositionResetMode.AUTO
+
+enum PositionResetMode {
+	AUTO,
+	TRUE,
+	FALSE
+}
 
 ##flag for allowing antenna program position changes to be persistent across resets
 var should_update_origin = false 
@@ -13,6 +22,8 @@ var should_update_origin = false
 func _ready() -> void:
 	sequencer_control_scene_UID = "uid://cwm6kjroqw6hn"
 	super._ready()
+	if should_reset_position == PositionResetMode.FALSE:
+		should_update_origin = true
 
 func initialize_screen(screen_scene: MoveCartScreen):
 	super.initialize_screen(screen_scene)
@@ -21,9 +32,11 @@ func initialize_screen(screen_scene: MoveCartScreen):
 func run() -> void:
 	for obstacle in obstacles:
 		obstacle.request_offbeat_action.emit(obstacle, action, should_update_origin)
-	should_update_origin = false
+	if should_reset_position == PositionResetMode.AUTO:
+		should_update_origin = false
 
 func _on_direction_selected(direction: Enums.Direction):
-	should_update_origin = true
+	if should_reset_position == PositionResetMode.AUTO:
+		should_update_origin = true
 	action = Enums.direction_to_action(direction)
 	run()
