c5a199b1df0d914cff0d0abb027a8962dd6429b3
jhol0613 <john.hollandr@gmail.com>
2026-06-19 22:34:53 +0300

    interactables now highlighted until interacted

--- godot_project/agents/interactables/interactable.gd
+++ godot_project/agents/interactables/interactable.gd
@@ -19,7 +19,10 @@ signal interaction_succeeded
 @export var follow_on_animation_on_success := "idle_down"
 ##Note that a horizontal flip won't appear in the editor
 @export var flip_horizontal := false: set = _on_flip_horizontal
-	
+##Highlight shader can't be used on animated sprite, so it uses this as a stand-in for its shape
+@export var highlight_sprites: Array[Sprite2D]
+var highlight_shader
+
 var _interacted = false
 
 signal updated_position(this_interactable: Interactable, new_global_position: Vector2)
@@ -28,6 +31,11 @@ signal updated_position(this_interactable: Interactable, new_global_position: Ve
 func _ready() -> void:
 	add_to_group("interactables")
 	_on_flip_horizontal(flip_horizontal)
+	highlight_shader = ShaderMaterial.new()
+	highlight_shader.shader = load(Enums.Shaders.HIGHLIGHT_GLINT)
+	highlight_shader.set_shader_parameter("offset", AudioManager.beat_time_seconds * 16)
+	for highlight_sprite in highlight_sprites:
+		highlight_sprite.material = highlight_shader
 	super._ready()
 
 #Override in children if additional flip functionality desired (e.g. cat coffee spill spawn location)
@@ -43,6 +51,8 @@ func execute_action(action : Enums.PlayerAction, beat: int, skip_animation := fa
 	if can_interact():
 		interaction_succeeded.emit()
 		_interacted = true
+		for highlight_sprite in highlight_sprites:
+			highlight_sprite.visible = can_interact()
 		super.execute_action(action, beat, skip_animation)
 
 func is_in_range(interact_position: Vector2i) -> bool:
@@ -59,4 +69,6 @@ func update_player_animation_based_on_position(player_grid_position: Vector2i):
 func reset():
 	super.reset()
 	_interacted = false
+	for highlight_sprite in highlight_sprites:
+		highlight_sprite.visible = can_interact()
 	sprite.play_with_signals(default_animation)
